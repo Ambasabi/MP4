a993eca723989da4ed092c09a75f62585280efe8
Daniel Prescott <DanielEPrescott@gmail.com>
2018-04-20 21:02:47 -0600

    maybe part b works

--- src/spouts/FileReaderSpout.py
+++ src/spouts/FileReaderSpout.py
@@ -12,14 +12,28 @@ class FileReaderSpout(Spout):
 
         # TODO:
         # Task: Initialize the file reader
+        with open(datafile) as f:
+            lines = f.read().splitlines()
+        self.sentences = iter(lines)
+        f.close()
+        # self.f = datafile #this might need to be something like open(datafile) or something.
+        # self._conf = stormconf
+        # self._context = context
 
 
     def next_tuple(self):
         # TODO:
         # Task 1: read the next line and emit a tuple for it
+        # for line in self.f.readlines():
+        #     storm.logInfo("Emitting %s" % line)
+        #     storm.emit([line])
+        sentence = next(self.sentences)
         # Task 2: don't forget to sleep for 1 second when the file is
         #         entirely read to prevent a busy-loop
-        # Task 3: use the "self.logger.info(...)" function to print 1. the message received and 2. the message emitted 
+        sleep(0.1)
+        # Task 3: use the "self.logger.info(...)" function to print 1. the message received and 2. the message emitted
+        self.emit([sentence])
+        self.logger.info("- [pid={}] - Emitting: spout [{}]".format(self.pid,sentence))
 
         pass
 
--- topologies/TopWordFinderTopologyPartB.py
+++ topologies/TopWordFinderTopologyPartB.py
@@ -11,8 +11,11 @@ class TopWordFinderTopologyPartA(Topology):
     # Task: wire up the topology
     # Make sure you use the following names for each component
     # FileReaderSpout -> "spout"
+    spout = FileReaderSpout.spec(inputs=config)
     # SplitSentenceBolt -> "split"
+    split = SplitSentenceBolt.spec(inputs={spout: Grouping.fields('word')})
     # WordCountBolt -> "count"
+    count = WordCountBolt.spec(inputs={split: Grouping.fields('word')})
 
     
 
