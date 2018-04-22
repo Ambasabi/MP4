e2af5a274f3694a0a18af6e71f3b5613e26f7f6d
Daniel Prescott <DanielEPrescott@gmail.com>
2018-04-22 11:54:49 -0600

    fixed normalizer maybe

--- src/bolts/NormalizerBolt.py
+++ src/bolts/NormalizerBolt.py
@@ -18,20 +18,9 @@ class NormalizerBolt(Bolt):
         # Task 1: make the words all lower case
         word = tup.values[0]
         lowerword = word.lower()
-        #lowersentence = tempsentence.lower()
-        # Task 2: remove the common words
-        # ret = []
-        # for word in lowersentence:
-        #     if word in self.common_words:
-        #         continue
-        #     else:
-        #         ret.append(word)
-        #     sentence = tuple(ret)
-        # if word in self.common_words:
-        #         return
-        # else:
         if lowerword not in self.common_words:
             self.logger.info("- [pid={}] - Processing received message [{}]".format(self.pid, word))
+            self.emit(lowerword)
             self.logger.info("- [pid={}] - Emitting: normalize [{}]".format(self.pid, lowerword))
 
 
